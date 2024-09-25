96552319ccfd7a747ab1e8f1c1831012736ec02e
usermlc <75613235+usermlc@users.noreply.github.com>
2024-09-26 00:38:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+// Serve static files, including favicon.ico
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Favicon route
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'favicon.ico'));
+});
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
